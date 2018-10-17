ad94f0d3eee0c46192d56b608c1db906c8f4864f
Ravi <ravimaharshi89@gmail.com>
2018-10-17 17:13:06 +0530

    minimum code to run oauth2 in memory with spring boot

--- src/main/java/com/oauth/angular/integration/ResourceServer.java
+++ src/main/java/com/oauth/angular/integration/ResourceServer.java
@@ -1,63 +1,48 @@
-/*package com.oauth.angular.integration;
+package com.oauth.angular.integration;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
 
 @EnableResourceServer
 @Configuration
 public class ResourceServer extends WebSecurityConfigurerAdapter {
-//ResourceServerConfigurerAdapter {
 
+	@Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Bean
+	public BCryptPasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	@Override
-	public void configure(ResourceServerSecurityConfigurer resources) {
-		resources.resourceId(RESOURCE_ID).stateless(false);
+	protected void configure(AuthenticationManagerBuilder auth)
+			throws Exception {
+		auth.parentAuthenticationManager(authenticationManagerBean())
+				.inMemoryAuthentication()
+				.withUser("Peter")
+				.password("$2a$10$qYI3w9mAT5F9urb2e3mfjecK.4zfAgQJpRlxGAUy4lop4bDKoH3DW") // encrypted value of peter
+				.roles("USER");
 	}
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-        http.
-                anonymous().disable()
-                .authorizeRequests()
-                .antMatchers("/users/**").authenticated()
-                .and().exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());
+		http.anonymous().disable().authorizeRequests()
+				.antMatchers("/rest/hello/**").authenticated().and()
+				.exceptionHandling()
+				.accessDeniedHandler(new OAuth2AccessDeniedHandler());
+
 	}
 
-	
-	
-	
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-    	 http.
-         anonymous().disable()
-         .authorizeRequests()
-         .antMatchers("/users/**").authenticated()
-         .and().exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());
-    	
-        }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.parentAuthenticationManager(authenticationManager)
-                .inMemoryAuthentication()
-                .withUser("Peter")
-                .password("peter")
-                .roles("USER");
-    }
-
-       @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/auth/token/");
-    }
-	@Override
-	public void configure(ResourceServerSecurityConfigurer resources) {
-		resources.resourceId(RESOURCE_ID).stateless(false);
-	}
-    
-}*/
+}
